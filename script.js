85350391a539ab0f50fd5e6c291d7cdeb3f6869e
AMRESH-SLACK <67509274+AMRESH-SLACK@users.noreply.github.com>
2020-07-18 19:55:14 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function summer(){
+    alert("Hey ! It's Summer");
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')";
+    document.body.style.backgroundSize= "100%";
+    document.body.style.backgroundRepeat="no-repeat";
+}
+function winter(){
+    alert("Hey ! It's Winter");
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+    document.body.style.backgroundSize= "100%";
+    document.body.style.backgroundRepeat="no-repeat";
+}
+function autmn(){
+    alert("Hey ! It's Autmn");
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backgroundSize= "100%";
+    document.body.style.backgroundRepeat="no-repeat";
+}
+function spring(){
+   alert("Hey ! It's Spring");
+   document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')" ;
+   document.body.style.backgroundSize= "100%";
+   document.body.style.backgroundRepeat="no-repeat";
+}
+function copy(){
+    alert("Lol ! You Cannot Copy")
+}
